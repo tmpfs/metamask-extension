84c268435bdde0ec76171a3e9528ea4985a4f9cb
muji <muji@tmpfs.org>
2021-05-28 11:53:27 +0800

    Testing entry points.

--- spack.config.js
+++ spack.config.js
@@ -1,6 +1,53 @@
 const path = require('path');
 const { config } = require('@swc/core/spack')
 
+
+module.exports = config([{
+  entry: {
+    //'main': __dirname + '/stories.js',
+    'main': __dirname + '/main.js',
+  },
+  output: {
+    path: path.join(__dirname, "builds", "stories"),
+  },
+  module: {},
+}]);
+
+//module.exports = config({
+  //entry: {
+    //'main': __dirname + '/.storybook/main.js',
+  //},
+  //output: {
+    //path: path.join(__dirname, "builds", "storybook"),
+  //},
+  //module: {},
+//});
+
+/*
+module.exports = config({
+  entry: {
+    'main': require.resolve('ses/lockdown'),
+  },
+  output: {
+    path: path.join(__dirname, "builds", "sesLockdown"),
+  },
+  module: {},
+});
+*/
+
+/*
+module.exports = config({
+  entry: {
+    'main': __dirname + '/app/scripts/runLockdown.js',
+  },
+  output: {
+    path: path.join(__dirname, "builds", "runLockdown"),
+  },
+  module: {},
+});
+*/
+
+/*
 module.exports = config({
   entry: {
     'main': __dirname + '/ui/index.js',
@@ -10,3 +57,52 @@ module.exports = config({
   },
   module: {},
 });
+*/
+
+/*
+module.exports = config({
+  entry: {
+    'main': __dirname + '/app/scripts/background.js',
+  },
+  output: {
+    path: path.join(__dirname, "builds", "background"),
+  },
+  module: {},
+});
+*/
+
+/*
+module.exports = config({
+  entry: {
+    'main': __dirname + '/app/scripts/inpage.js',
+  },
+  output: {
+    path: path.join(__dirname, "builds", "inpage"),
+  },
+  module: {},
+});
+*/
+
+/*
+module.exports = config({
+  entry: {
+    'main': __dirname + '/app/scripts/contentscript.js',
+  },
+  output: {
+    path: path.join(__dirname, "builds", "contentscript"),
+  },
+  module: {},
+});
+*/
+
+/*
+module.exports = config({
+  entry: {
+    'main': __dirname + '/app/scripts/initSentry.js',
+  },
+  output: {
+    path: path.join(__dirname, "builds", "initSentry"),
+  },
+  module: {},
+});
+*/
